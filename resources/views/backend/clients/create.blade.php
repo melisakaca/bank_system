ffb3eb8a9a214c0c609ba8783a224b11426211f6
melisakaca <melisakaca23@gmai.com>
2025-02-18 22:46:15 +0900

    fix layout

--- resources/views/backend/clients/create.blade.php
+++ resources/views/backend/clients/create.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.master-new')
 
 @section('content')
-    <h1>Create Client</h1>
-    <form action="{{ route('clients.store') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" required>
-        </div>
-        <button type="submit" class="btn btn-primary mt-4">Create</button>
-    </form>
+    <div class="container">
+        <h1>Create Client</h1>
+        <form action="{{ route('clients.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password_confirmation">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"
+                    required>
+            </div>
+            <button type="submit" class="btn btn-primary mt-4">Create</button>
+        </form>
+    </div>
 @endsection
